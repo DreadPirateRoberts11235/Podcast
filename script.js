7e809957dd2ac4d807373c3f6f686049d6800715
DreadPirateRoberts11235 <147689264+DreadPirateRoberts11235@users.noreply.github.com>
2023-10-12 00:47:47 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// JavaScript for podcast playback
+
+// Get the audio element
+const audioPlayer = document.getElementById("audio-player");
+
+// Check if the audio element is supported
+if (audioPlayer) {
+    // Set the podcast audio source
+    audioPlayer.src = "your-podcast-audio.mp3";
+}
